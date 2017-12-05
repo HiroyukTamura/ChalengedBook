fe6171a0ba5af43b0a7c99280ea437ade25beb26
hiroyuki <hiroyukitamura@gmail.com>
2017-12-06 01:47:35 +0900

    苦しみそのもの

--- sample/basic/js/basic.js
+++ sample/basic/js/basic.js
@@ -197,6 +197,181 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+function init() {
+    // var errDiv = document.getElementById('err_div');
+
+    var key = getParameterByName('key');
+    if (!key) {
+        // document.getElementById('placeholder').style.display = 'none';
+        // errDiv.style.display = 'inline';
+        document.title = 'エラー';
+        alert('処理に失敗しました');
+        return;
+    }
+
+    firebase.database().ref('books/' + key).once('value').then(function (snapshot) {
+        if (!snapshot.exists()) {
+            // document.getElementById('placeholder').style.display = 'none';
+            // errDiv.style.display = 'inline';
+            console.log('error 124');
+            document.title = 'エラー';
+            alert('処理に失敗しました');
+            return;
+        }
+
+        var name = snapshot.child('name').val();
+        document.title = name + 'さんのトリセツ';
+        document.getElementById('title').innerHTML = snapshot.child('name').val() + "さんの<br>トリセツ";
+        document.getElementById('sub_title').innerHTML = 'Written at ' + snapshot.child('date').val();
+
+        //var json = <?php //echo $json = json_encode($value, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT); ?>// ;
+        var json = [];
+        json[0] = snapshot.child('aboutDisease').val();
+        json[1] = snapshot.child('detailDisease').val();
+        json[2] = snapshot.child('consideration').val();
+        json[3] = snapshot.child('aboutMe').val();
+        json[4] = snapshot.child('msg').val();
+
+        console.log(json);
+
+        var flipBook = document.getElementsByClassName('flipbook')[0];
+
+        var i = 0;
+
+        json.forEach(function (value) {
+            value = value.replace('\n', '<br>');
+        });
+
+        while (i < 5) {
+            console.log(json[i]);
+
+            if (!json[i]) {
+                i++;
+                console.log('きゃっちされました');
+                continue;
+            }
+
+            //フレーム作成
+            var div = document.createElement('div');
+            div.style.backgroundImage = "url('pages/screen1.png')";
+            flipBook.appendChild(div);
+            var innerDiv = document.createElement('div');
+            div.appendChild(innerDiv);
+
+            //コンテンツ作成
+            var wrapper = makeContentsWrapper(getTitle(i), json[i]);
+            innerDiv.appendChild(wrapper);
+
+            if (wrapper.clientHeight < 600) {
+
+                i++;
+
+                // while (i <json.length -1){
+                //
+                //     if(!json[i]){
+                //         i++;
+                //         console.log('きゃっちされました');
+                //         continue;
+                //     }
+                //
+                //     var nextWrapper = makeContentsWrapper(getTitle(i), json[i]);
+                //     innerDiv.appendChild(nextWrapper);
+                //     console.log(nextWrapper.parentElement.clientHeight);
+                //
+                //     var count = 0;
+                //     if(nextWrapper.parentElement.clientHeight >= 600) {
+                //         console.log('innerDiv.clientHeight >= 600' + json[i]);
+                //         nextWrapper.parentNode.removeChild(nextWrapper);
+                //         count++;
+                //         if(count > 100){
+                //             alert('処理に失敗しました');
+                //             return;
+                //         }
+                //         break;
+                //     }
+                //
+                //     i++;
+                // }
+
+            } else {
+                console.log('こっちやね');
+
+                //ひとコンテンツではみ出してしまう。よって、分割が必要だ。
+                var str = json[i];
+                var content = wrapper.getElementsByClassName('contents')[0];
+                while (wrapper.clientHeight + 100 > 600) {
+                    str = str.slice(0, -5);
+                    content.innerHTML = str;
+                    console.log(wrapper.clientHeight);
+                }
+
+                //残りの文字を配列に格納し直す
+                json[i] = json[i].slice(str.length + 1);
+                str = json[i];
+
+                while (json[i].length !== 0) {
+                    console.log('162' + str);
+
+                    //枠組みとなるdivを生成
+                    var divS = document.createElement('div');
+                    divS.style.backgroundImage = "url('pages/screen1.png')";
+                    // divS.className = 'contents_wrapper';
+                    var innerDivS = document.createElement('div');
+                    innerDivS.className = 'contents_wrapper';
+                    divS.appendChild(innerDivS);
+
+                    //コンテンツを格納
+                    var strS = json[i];
+                    var contentExtra = document.createElement('p');
+                    contentExtra.className = 'contents';
+                    contentExtra.innerHTML = strS;
+                    innerDivS.appendChild(contentExtra);
+
+                    //本体にappend
+                    flipBook.appendChild(divS);
+
+                    while (innerDivS.clientHeight + 100 > 600) {
+                        str = str.slice(0, -5);
+                        content.innerHTML = str;
+                        console.log('うむ！' + wrapper.clientHeight);
+                    }
+
+                    json[i] = json[i].slice(str.length);
+                    str = json[i];
+                }
+
+                i++;
+            }
+        }
+
+        //本は偶数ページ数でなければいけないから、その場合は1頁増やす
+        if (flipBook.childNodes.length % 2 === 0) {
+            flipBook.insertAdjacentHTML('beforeend', "<div style=\"background-image:url(pages/screen1.png)\"></div>");
+            console.log('まさかの');
+        }
+
+        flipBook.insertAdjacentHTML('beforeend', "<div class=\"hard\" style=\"background-color : #f2f2f2\"></div>" +
+            "\n" +
+            "<div class=\"hard\" style=\"background-color : #f2f2f2\"></div>");
+
+        console.log('259');
+        // document.getElementById('placeholder').style.display = 'none';
+        document.getElementsByClassName('flipbook-viewport')[0].style.display = 'inline';
+    });
+}
+
+function initFirebase() {
+    var config = {
+        apiKey: "AIzaSyBQnxP9d4R40iogM5CP0_HVbULRxoD2_JM",
+        authDomain: "wordsupport3.firebaseapp.com",
+        databaseURL: "https://wordsupport3.firebaseio.com",
+        projectId: "wordsupport3",
+        storageBucket: "wordsupport3.appspot.com",
+        messagingSenderId: "60633268871"
+    };
+    firebase.initializeApp(config);
+}
+
 // function createContentsWrapper(attrs, i) {
 //     var wrapper = document.createElement('div');
 //     wrapper.className = 'contents_wrapper';
